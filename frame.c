8e3c1f07a070114107c38444756147fe07ab1520
00519575 <andrealorenzobruno.villa@telecomitalia.it>
2022-02-24 13:56:34 +0100

    .

--- frame.c
+++ frame.c
@@ -1,17 +1,59 @@
 
 
 #include "snapshot.h"
-#include "autocorr.c"
 #include "macros.h"
 
 
+
 float lerp( float a , float b , float x ){ return a+(b-a)*x; }
+
+
+
+
+
+
+void autocorr( float *out , int out_len , int lag_start , float *in ){
+  // naive impl. O(n²)
+  // non è propriamente autocorrelazione
+  // nel senso che cosi calcolata l'autocorr massima = 0
+  // in letteratura dovrebbe essere +1 ... ma chissene
+  // per i miei scopi è indifferente
+  for(int lag=lag_start;lag<out_len;lag++){
+    float sqdiff = 0;
+    for(int i=0;i<out_len;i++){
+      float diff = in[i]-in[i+lag];
+      sqdiff += diff*diff;
+    }
+//    out[lag] = sqdiff/(out_len-lag_start);  // scalatura "giusta" ... gole pessime
+//    sqdiff=sqrt(sqdiff);  // sarebbe più corretto, ma farlo genera gole meno accentuate
+    out[lag] = sqdiff/(1+sqdiff);  // scalatura quasi-log (reinhard) aiuta nel delineare le gole
+  }
+}
+
+
+
+void circorr( float *out , int out_len , float *in ){
+  // autocorrelazione circolare
+  for(int lag=0;lag<out_len;lag++){
+    float sqdiff = 0;
+    for(int i=0,j=lag;i<out_len;i++,j++){
+      while(j>=out_len)j-=out_len;
+      float diff = in[i]-in[j];
+      sqdiff += diff*diff;
+    }
+    out[lag] = sqdiff/(1+sqdiff);  // scalatura quasi-log (reinhard) aiuta nel delineare le gole
+  }
+}
+
+
+
 
 
 void frame( SDL_Event event ){
 
 //  pix( rand()%WID , rand()%HGT, rand()%256 , rand()%256 , rand()%256 );
 //  pix( event.motion.x , event.motion.y , 255 , 255 , 255 );
+
 
   SDL_SetRenderDrawColor( renderer , 0,0,0 , 255 );
   SDL_RenderClear(renderer);
@@ -21,47 +63,14 @@ void frame( SDL_Event event ){
   float snap[WID];
   snapshot_rd(snap,COUNT(snap));
 
-
-
-  // l'idea come sempre è di minimizzare i calcoli
-  // l'autocorrelazione viene calcolata in un certo range di lag
-  // poi si cerca la prima gola intorno allo 0
-  // partendo da lag 0
-
 #define SR 11025
+  float acor[SR/60]={0};  // min Hz (=max lag)
+  autocorr(acor, COUNT(acor) , 0 , snap);
+//  autocorr(acor, COUNT(acor) , SR/1000 , snap);
+  //                           ^^^max Hz (=min lag)
 
-  static int min_lag;
-  static int max_lag;
-  static int notch_lag = 0;
-
-  if(notch_lag==0)
-  {
-    min_lag = SR/600;
-    max_lag = SR/60;
-  }
-
-  float acor[SR/60]={0};
-  float avg = autocorr( acor, max_lag , min_lag , snap );
-
-  if(avg<0.1)
-  {
-    notch_lag=0;    
-  }
-  else  
-  {
-    // troviamo il minimo assoluto intorno a 0
-    int cand_min=10;
-    for(int i=min_lag;i<max_lag;i++)
-    {
-      if(cand_min<acor[i])continue; // cerchiamo il minimo
-      if(acor[i]>0.1)continue;  // tra 0 e ...
-      cand_min=acor[i];
-      notch_lag=i;
-    }
-    min_lag=notch_lag-5;
-    max_lag=notch_lag+5;
-  }
-
+  float ccor[COUNT(acor)]={0};
+  circorr(ccor, COUNT(ccor) , snap);
 
   for(int x=0;x<WID;x++)
   {
@@ -70,28 +79,25 @@ void frame( SDL_Event event ){
 
     SDL_Rect rectangle;
     rectangle.x = x;
-    rectangle.y = lerp(HGT/4,0,v);
+    rectangle.y = lerp(HGT/6,0,v);
     rectangle.w = 1;
-    rectangle.h = lerp(0,HGT/2,v);
+    rectangle.h = lerp(0,HGT/3,v);
     SDL_RenderFillRect(renderer,&rectangle);
 
-    if(x>=COUNT(acor))continue;
-    v = acor[x];
-    rectangle.y = lerp(HGT,HGT/2,v);
-    rectangle.h = lerp(0,HGT/2,v);
-    SDL_RenderFillRect(renderer,&rectangle);
+    if(x<COUNT(acor)){
+      v = acor[x];
+      rectangle.y = lerp(HGT*2/3,HGT*1/3,v);
+      rectangle.h = lerp(0,HGT/3,v);
+      SDL_RenderFillRect(renderer,&rectangle);
+    }
+
+    if(x<COUNT(ccor)){
+      v = ccor[x];
+      rectangle.y = lerp(HGT*3/3,HGT*2/3,v);
+      rectangle.h = lerp(0,HGT/3,v);
+      SDL_RenderFillRect(renderer,&rectangle);
+    }
   }
-
-  SDL_SetRenderDrawColor( renderer , 255,0,0 , 255 );
-
-  SDL_Rect rectangle;
-  rectangle.x = min_lag;
-  rectangle.y = HGT*3/4;
-  rectangle.w = 1;
-  rectangle.h = HGT*1/4;
-  SDL_RenderFillRect(renderer,&rectangle);
-  rectangle.x = max_lag;
-  SDL_RenderFillRect(renderer,&rectangle);
 }
 
 
